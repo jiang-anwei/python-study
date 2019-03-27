3fc6c68d5e1314852b4f89a4ee08158916db5feb
jianganwei <jiang_anwei@icekredit.com>
2019-03-27 17:41:27 +0800

    celery

--- celery/product.py
+++ celery/product.py
@@ -6,7 +6,7 @@ addresult = [add_task.delay(i, i) for i in range(10)]
 mult_result = [multiplication_task.delay(i, i) for i in range(20)]
 
 for re in addresult:
-    print re.get()
+    print "加法：", re.get()
 for re in mult_result:
-    print re.get()
+    print "乘法：", re.get()
 print "cost:{}".format(str(time.time() - start))
